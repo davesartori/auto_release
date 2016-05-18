b716858ee85a40c729b58a4c61c7c6202572932f
p654739 <david.sartori@nab.com.au>
2016-05-18 20:22:10 +1000

    daft

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-/Users/davidsartori/Documents/projects/nab/gradle_auto_release/gradlew clean build
-gradle autorelease
+cd /Users/davidsartori/Documents/projects/nab/gradle_auto_release
+./gradlew clean build
+cd /Users/davidsartori/Google Drive/projects/personal/auto_release2
+./gradlew autorelease
 
